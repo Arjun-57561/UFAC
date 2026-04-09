4be8f1b7bbd09913919a038198f1cad7e5dd1831
Arjun Reddy <160053005+Arjun-57561@users.noreply.github.com>
2026-04-10 02:52:37 +0530

    updated rag pipeline

--- data/rag_pipeline.py
+++ data/rag_pipeline.py
@@ -5,7 +5,8 @@ import os
 import logging
 from typing import Optional
 from langchain_community.document_loaders import PyPDFDirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import SentenceTransformerEmbeddings
 
